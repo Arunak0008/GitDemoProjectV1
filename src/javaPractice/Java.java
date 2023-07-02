43f97dc4cd550357390eb73ceb23f4dcd6fe3502
ELCOT <arunganesh04@gmail.com>
2023-07-02 19:43:26 +0530

    updated

---
+++ src/javaPractice/Java.java
@@ -0,0 +1,15 @@
+package javaPractice;
+
+public class Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String hungry = "yes";
+       if(hungry =="yes")
+       System.out.println("order a pizza");
+       else {
+    	   System.out.println("no need to order");
+       }
+	}
+
+}
